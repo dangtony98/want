baff39e695805bb02b258a3e96ba60786f210a5d
Tuan Dang <dangtony98@gmail.com>
2019-02-08 16:32:29 -0500

    Delete all old chat widget components

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ModalHandler from '../modal/ModalHandler';
 import NavigationBar from '../navigation/NavigationBar';
 import HomeContent from '../wrappers/HomeContent';
-import ChatWidget from '../chat/ChatWidget';
-import ChatBox from '../chat/ChatBox';
 
 export default () => {
     window.scrollTo(0, 0);
